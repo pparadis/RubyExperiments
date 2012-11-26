2811a3f8798dfde48c7b33e39d1d079d2e993885
Pascal Paradis <pascal.paradis@gmail.com>
2012-11-25 21:49:44 -0500

    Création d'une image

--- hi.rb
+++ hi.rb
@@ -1,13 +1,24 @@
 require 'sinatra'
+require 'base64'
+require 'haml'
+require 'securerandom'
 
-get '/hi' do
-  "Hello World!"
-end
+set :static, true
+set :public_folder, '/uploads'
 
 get '/' do
-  "Home"
+  haml :index
+end
+
+get '/uploads/:file' do |f|
+	send_file 'uploads/' + f
 end
 
 post '/image' do
-  "Home"
+	hostUrl = 'http://' + request.host_with_port()
+	filePath = 'uploads/' + params['imageName']+SecureRandom.uuid.gsub('-','') + ".png"
+	File.open(filePath, "wb") do |f|
+		f.write(Base64.decode64(params['imageData']))
+	end
+	return 'http://' + request.host_with_port() + '/' + filePath
 end
